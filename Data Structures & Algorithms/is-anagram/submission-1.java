1ef9209544cf17373fbc9143c5fec5054b7cecb3
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-07-31 22:53:54 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx1 = s.charAt(i) - 'a';
+            int idx2 = t.charAt(i) - 'a';
+            freq[idx1]++;
+            freq[idx2]--;
+        }
+
+        for (int ele : freq) if (ele != 0) return false;
+        return true;
+    }
+}
