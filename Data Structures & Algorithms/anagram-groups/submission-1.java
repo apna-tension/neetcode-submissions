b5652f89ea22b3838d7103a471ffbfed0795dc9a
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-01 09:37:32 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        List<List<String>> list = new ArrayList<>();
+
+        for (String str : strs) {
+            int[] fr = new int[26];
+            for (char ch : str.toCharArray()) {
+                fr[ch-'a']++;
+            }
+            String frS = Arrays.toString(fr);
+            if (!map.containsKey(frS)) {
+                map.put(frS, new ArrayList<>());
+                set.add(frS);
+            }
+            map.get(frS).add(str);
+        }
+        
+        // for (String s : set) {
+        //     List<String> ds = new ArrayList<>();
+        //     for (String str : map.get(s)) {
+        //         ds.add(str);
+        //     }
+        //     list.add(new ArrayList<>(ds));
+        // }
+
+
+        for (Map.Entry<String,List<String>> mapElement : map.entrySet()) {
+            // String key = mapElement.getKey();
+
+            // Adding some bonus marks to all the students
+            List<String> ds = new ArrayList<>();
+            for (String str : mapElement.getValue()) {
+                ds.add(str);
+            }
+
+            // Printing above marks corresponding to
+            // students names
+            list.add(new ArrayList<>(ds));
+        }
+        return list;
+    }
+}
