3520b95ad1d99711662456fead24d86361ba1d06
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-07 23:08:06 +0530

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String st : tokens) {
+            if (Character.isDigit(st.charAt(st.length()-1))) {
+                stack.push(Integer.parseInt(st));
+            } else {
+                int right = stack.pop();
+                int left = stack.pop();
+                if (st.equals("+")) {
+                    stack.push(left+right);
+                } else if (st.equals("*")) {
+                    stack.push(left*right);
+                } else if (st.equals("-")) {
+                    stack.push(left - right);
+                } else {
+                    stack.push(left / right);
+                }
+            }
+            // System.out.println(stack);
+        }
+        return stack.pop();
+    }
+}
