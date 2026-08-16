37fd9b99ef07aaf2a24cd0b5886a7c37d80ed707
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-16 16:10:19 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
+        for (int ele : nums) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+            set.add(ele);
+        }
+
+
+        Map<Integer, ArrayList<Integer>> map2 = new HashMap<>();
+        for (int ele : set) {
+            int freq = map.get(ele);
+            if (!pq.contains(freq)) pq.offer(freq);
+            if (!map2.containsKey(freq)) map2.put(freq, new ArrayList<>());
+            map2.get(freq).add(ele);
+        }
+
+        // System.out.println(pq);
+        // System.out.println(map2);
+
+        int[] ans = new int[k];
+        while (k != 0) {
+            List<Integer> list = map2.get(pq.poll());
+            for (int ele : list) {
+                ans[k-1] = ele;
+                k--;
+                if (k == 0) return ans;
+            }
+        }
+
+        return ans;
+    }
+
+}
