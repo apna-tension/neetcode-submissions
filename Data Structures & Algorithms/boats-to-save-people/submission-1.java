185571fb1a12a1fc9db92f07ec76b119a8fdc632
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-19 09:25:16 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length-1;
+        int count = 0;
+        while (i < j) {
+            if (people[i] + people[j] <= limit) i++;
+            count++;
+            j--;
+        }
+        if (i == j) count++;
+        return count;
+    }
+}
