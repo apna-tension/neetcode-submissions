73b24d57ab167f5e07fe6233523fc633dad11d21
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-22 01:25:50 +0530

    Add: asteroid-collision - submission-9

---
+++ Data Structures & Algorithms/asteroid-collision/submission-9.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int ele : asteroids) {
+            
+            while (!stack.isEmpty() && ele < 0 && stack.peek() > 0) {
+                // int diff = ele + stack.peek();
+                if (Math.abs(ele) > stack.peek()) stack.pop();
+                else if (Math.abs(ele) < stack.peek()) ele = 0;
+                else {
+                    stack.pop();
+                    ele = 0;
+                }
+            }
+
+            if (ele != 0) stack.push(ele);
+        }
+        // return new int[]{0};
+
+
+
+        int[] arr = new int[stack.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[arr.length-1-i] = stack.pop();
+        }
+        // int[] arr = stack.toArray();
+        return arr;
+        
+    }
+}
