fc25df514e453148c8d69a81440df9d874e71701
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-27 22:17:42 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int e = Integer.MIN_VALUE;
+
+        for (int ele : piles) e = Math.max(e, ele);
+        if (h == piles.length) return e;
+
+        int s = 1;
+        int min = e;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            long hrs = 0;
+
+            for (int ele : piles) {
+                hrs += Math.ceil((double)ele / mid);
+            }
+
+            // if (hrs == h) return mid;
+            if (hrs <= h) {
+                min = Math.min(min, mid);
+                e = mid - 1;
+            } else s = mid + 1;
+        }
+        return min;
+    }
+}
