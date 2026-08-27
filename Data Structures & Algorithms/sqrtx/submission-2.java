55c7457f573572a8bcd215003a5c86d99588129d
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-08-27 20:06:00 +0530

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        long s = 0;
+        long e = x;
+        while (s <= e) {
+            long mid = s + (e - s) / 2;
+            // System.out.println("hit : " + mid + " : " + s + " : " + e);
+            if ((long)(mid * mid) == (long)x) return (int)mid;
+            if ((long)(mid * mid) > (long)x) e = mid - 1;
+            else s = mid + 1;
+        }
+        return (int)e;
+    }
+}
