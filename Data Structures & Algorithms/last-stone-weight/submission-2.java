6d6ee3e9d86740c40c1e57249b1ccaa144f07049
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-09-14 19:47:41 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int ele : stones) pq.offer(ele);
+
+        while (pq.size() > 1) {
+            int y = pq.poll();
+            int x = pq.poll();
+            if (x < y) {
+                pq.offer(y-x);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
