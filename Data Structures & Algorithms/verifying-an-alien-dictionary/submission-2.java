5d9ddbd97a0a2f4472286f187b71c7b14d28e9da
aminul islam <77560208+apna-tension@users.noreply.github.com>
2026-09-24 17:34:26 +0530

    Add: verifying-an-alien-dictionary - submission-2

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            map.put(order.charAt(i), i);
+        }
+        System.out.println(map);
+        for (int i = 0; i < words.length-1; i++) {
+            if (!big(words[i], words[i+1], map)) return false;
+        }
+        return true;
+    }
+
+    private boolean big(String a, String b, Map<Character, Integer> map) {
+        
+        int i = 0;
+        int j = 0;
+        while (i < a.length() && j < b.length()) {
+            int n1 = map.get(a.charAt(i));
+            int n2 = map.get(b.charAt(j));
+            if (n1 > n2) return false;
+            
+            if (n1 < n2) return true;
+            i++;
+            j++;
+        }
+
+        if (i != a.length()) return false;
+        return true;
+    }
+}
